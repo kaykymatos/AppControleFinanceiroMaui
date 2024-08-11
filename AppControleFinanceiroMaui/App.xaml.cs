ee0c9615139aa03920cdec410c55bae5f8a3b510
Kayky Matos Santana <kayky7277@gmail.com>
2024-08-11 14:17:29 -0300

    first commit

---
+++ AppControleFinanceiroMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppControleFinanceiroMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
